8d8daff350710fab34ef4f9da6211d0eb04dda4d
Vinay yellaram <vinayyellaram715@gmail.com>
2026-04-07 16:23:23 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+
+        let maxHeight = 0;
+        let right = heights.length - 1;
+        let left = 0;
+
+        while(left < right){
+
+            let newLen = right - left;
+
+            let min = Math.min(heights[left],heights[right])
+
+            console.log(heights[left] ,heights[right], min,newLen,right , left , maxHeight)
+
+
+            maxHeight = Math.max(maxHeight , min * newLen);
+
+            if(heights[left] < heights[right]){
+                left++;
+            }else if (heights[left] == heights[right]){
+                right--;
+            }else{
+                right--;
+            }
+
+        }
+
+        return maxHeight;
+    }
+}
